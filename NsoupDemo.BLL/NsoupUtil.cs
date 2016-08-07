c09a1566c3df88f9acaf8ff83f5a7791682fd198
Sean <466374952@qq.com>
2016-08-07 15:10:08 +0800

    添加项目文件。

---
+++ NsoupDemo.BLL/NsoupUtil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NsoupDemo.BLL
+{
+    /// <summary>
+    /// Nsoup 组件类   处理网页html
+    /// </summary>
+    public class NsoupUtil
+    {
+        //Url
+        private string url = "http://www.topsage.com/";//种子url
+
+        public string Url
+        {
+            get { return this.url; }
+            set { url = value; }
+        }
+
+        public NsoupUtil(string url)
+        {
+            this.url = url;
+        }
+        //连接Url  获取Document
+        /// <summary>
+        /// 获取网页Document
+        /// </summary>
+        /// <returns></returns>
+        public NSoup.Nodes.Document GetDocument()
+        {
+            NSoup.Nodes.Document doc = NSoup.NSoupClient.Connect(this.Url).Get();
+            return doc;
+        }
+
+
+    }
+}
---
+++ NsoupDemo.Tests/NsoupUtilTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NsoupDemo.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NsoupDemo.BLL.Tests
+{
+    [TestClass()]
+    public class NsoupUtilTests
+    {
+        [TestMethod()]
+        public void GetDocumentTest(string url)
+        {
+            NsoupUtil nsoup = new NsoupUtil(url);
+
+            var doc = nsoup.GetDocument();
+            //Console.WriteLine();
+            Assert.IsNotNull(doc);
+        }
+    }
+}
