31e3312ec064ee9c6b53102d54d8a295e676cb79
Sean <466374952@qq.com>
2016-08-14 14:55:03 +0800

    NUnit.Tests

---
+++ NUnit.Tests/NUnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using NsoupDemo.BLL;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        //支持参数化测试
+        [TestCase(3)]  
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(0)]
+        public void TestMethod1(int length)
+        {
+            var result = RandomCodeHelper.GenerateRandomCode(length);
+            Console.WriteLine(length+" "+result);
+            Assert.AreEqual(result.Length, length);
+        }
+
+
+
+    }
+}
---
+++ NsoupDemo.Tests/RandomCodeHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NsoupDemo.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NsoupDemo.BLL.Tests
+{
+    [TestClass()]
+    public class RandomCodeHelperTests
+    {
+        [TestMethod()]
+        public void GenerateRandomCodeTest(int length)
+        {
+            RandomCodeHelper.GenerateRandomCode(length);
+            Assert.Fail();
+        }
+    }
+}
